58c01201ef69f4ea981330e08b491188d6d47d17
Susruthad <susruthanewid@gmail.com>
2024-02-19 17:05:07 -0800

    Initial Commit

---
+++ Lab-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab-6
+//
+//  Created by user240733 on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
